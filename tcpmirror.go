bb5b610fdcd4a91de22c3d6c078d12a5c224a855
Sergey Ponomarev <stokito@gmail.com>
2021-12-12 23:24:17 +0200

    Mirror outgoing traffic

--- tcpmirror.go
+++ tcpmirror.go
@@ -23,6 +23,9 @@ var (
 	mirrorPtr = flag.String("m", "localhost:9091",
 		"Mirror incoming traffic to `host:port[,host:port]...`. Can specify multiple addresses seperated by a comma. Eg. localhost:9091,localhost:9092")
 
+	mirrorRespPtr = flag.String("r", "localhost:9100",
+		"Mirror outgoing traffic to `host:port[,host:port]...`. Can specify multiple addresses seperated by a comma. Eg. localhost:9100,localhost:9101")
+
 	debugPtr = flag.Bool("d", false, "Print debug information on stdout")
 )
 
@@ -32,10 +35,12 @@ func main() {
 	flag.Parse()
 
 	mirrorAddrs := strings.Split(*mirrorPtr, ",")
+	mirrorRespAddrs := strings.Split(*mirrorRespPtr, ",")
 
 	fmt.Printf("Listening on                    %s\n", *listenPtr)
 	fmt.Printf("Connecting in primary mode to   %s\n", *primaryPtr)
 	fmt.Printf("Duplicating incoming traffic to %s\n", *mirrorPtr)
+	fmt.Printf("Duplicating outgoing traffic to %s\n", *mirrorRespPtr)
 
 	l, err := net.Listen("tcp", *listenPtr)
 	if err != nil {
@@ -58,6 +63,7 @@ func main() {
 		// - primary addr
 		// - stdout (if debug)
 		ws := make([]io.Writer, len(mirrorAddrs))
+		wsResp := make([]io.Writer, len(mirrorRespAddrs))
 
 		for i, mirrorAddr := range mirrorAddrs {
 			m, err := net.Dial("tcp", mirrorAddr)
@@ -71,8 +77,19 @@ func main() {
 
 		mw := io.MultiWriter(ws...)
 
+		for i, mirrorRespAddr := range mirrorRespAddrs {
+			m, err := net.Dial("tcp", mirrorRespAddr)
+			if err != nil {
+				fmt.Println("Error connecting to the mirror outgoing address: ", err.Error())
+				os.Exit(1)
+			}
+			wsResp[i] = m
+		}
+		wsResp = append(wsResp, in) // add primary
+		mwResp := io.MultiWriter(wsResp...)
+
 		go io.Copy(mw, in)
-		go io.Copy(in, p)
+		go io.Copy(mwResp, p)
 
 		// fmt.Println("After accept")
 		// fmt.Printf("mw = %v\nin = %v\n", mw, in)
@@ -83,8 +100,8 @@ func main() {
 
 func Usage() {
 	fmt.Fprintf(os.Stderr, "tcpmirror version %s\n", Version)
-	fmt.Fprintf(os.Stderr, "Usage:   $ tcpmirror -l <listen_addr> -p <primary_addr> -m <mirror_addrs\n")
-	fmt.Fprintf(os.Stderr, "Example: $ tcpmirror -l localhost:8080 -p localhost:9090 -m localhost:9091,localhost:9091 \n")
+	fmt.Fprintf(os.Stderr, "Usage:   $ tcpmirror -l <listen_addr> -p <primary_addr> -m <mirror_addrs> -r <mirror_outgoing_addrs>\n")
+	fmt.Fprintf(os.Stderr, "Example: $ tcpmirror -l localhost:8080 -p localhost:9090 -m localhost:9091,localhost:9091 -r localhost:9100,localhost:9101\n")
 	fmt.Fprintf(os.Stderr, "-----------------------\nFlags:\n")
 	flag.PrintDefaults()
 }
